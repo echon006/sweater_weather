5f2b81a6e4d668c9e20d92006efde5fa5f834eb0
Eric Chon <echon92@gmail.com>
2022-03-07 10:10:41 -0700

    Book facade test passing, book poro test and quantity pending

--- app/poros/book.rb
+++ app/poros/book.rb
@@ -8,15 +8,24 @@ class Book
   def initialize(weather_data, book_data, location)
     @destination = location
     @forecast = weather(weather_data.current_weather)
-    require "pry"; binding.pry
-    @total_books_found = book_data[]
-    @books = book_data[]
+    @total_books_found = book_data[:numFound]
+    @books = book_list(book_data[:docs])
   end
 
   def weather(data)
     {
-      summary: data[:conditions]
+      summary: data[:conditions],
       temperature: data[:temp]
     }
+  end
+
+  def book_list(data)
+    data.map do |book|
+      {
+        isbn: book[:isbn],
+        title: book[:title],
+        publisher: book[:publisher]
+      }
+    end
   end
 end
