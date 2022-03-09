02ea101142cdf438ee8016c0ccf5a59a3c292384
Eric Chon <echon92@gmail.com>
2022-03-08 17:20:17 -0700

    weather at eta working as intended

--- app/controllers/api/v1/road_trips_controller.rb
+++ app/controllers/api/v1/road_trips_controller.rb
@@ -5,7 +5,6 @@ class Api::V1::RoadTripsController < ApplicationController
   def create
     start_location = params[:origin]
     destination = params[:destination]
-
     if start_location.present? && destination.present?
       route = RoadTripFacade.find_route(start_location, destination)
       render json: RoadTripSerializer.new(route)
--- app/poros/road_trip.rb
+++ app/poros/road_trip.rb
@@ -10,15 +10,43 @@ class RoadTrip
     @start_city = start_location
     @end_city = destination
     @travel_time = route[:route][:formattedTime]
-    @weather_at_eta = weather(weather[:hourly])
+    @time_at_eta = estimated_time(weather[:current])
+    @weather_at_eta = weather(weather[:hourly][@time_at_eta])
   end
 
   def weather(data)
     {
-      temperature: data[0][:temp],
-      conditions: data[0][:weather][0][:description]
+      temperature: data[:temp],
+      conditions: data[:weather][0][:description]
     }
   end
+
+  def estimated_time(data)
+    if data != nil
+      total_hours = 0
+      start_route_time = Time.at(data[:dt]).strftime("%H:%M")
+      hour_route_time = start_route_time.split("")[0..1].join.to_i
+      minute_route_time = start_route_time.split("")[3..4].join.to_i
+      hour_travel_time = @travel_time.split("")[0..1].join.to_i
+      minute_travel_time = @travel_time.split("")[3..4].join.to_i
+      total_min = minute_route_time + minute_travel_time
+      total_hour = hour_route_time + hour_travel_time
+
+      if total_min >= 91
+        total_hours += 2
+      elsif total_min >= 30 && total_min <= 90
+        total_hours += 1
+      end
+      total_hours
+      arrival_hour = total_hours + total_hour
+      arrival_hour
+    else
+      0
+    end 
+  end
+
+
+
 
   # Get current time
   # add travel time
